b5211f7b63a2596b0394b225d0f7bedf790c28d4
Priyanka <33025640+priyanka2907@users.noreply.github.com>
2017-10-24 11:26:38 +0530

    Create Addition_Sub.php

---
+++ Addition_Sub.php
@@ -0,0 +1,50 @@
+<?php
+//add two variables in php
+$no1=2;//declare variables with value
+$no2=4;$ans;
+echo "PHP Basic Addition Subtraction Multiplication Division Program!<br><br>";
+
+$ans=$no1+$no2;// add variables
+
+echo "Addition Of Two Numbers is:-" .$ans;//print ans
+ echo "<br><br>";
+
+//subtraction of two nos.
+// print result on  new line 
+ 
+ 
+$sub=$no2-$no1;
+echo "Subtraction of two no is: " .$sub;
+ echo "<br><br>";
+//now multiplication of two nos.
+
+$multi=$no1* $no2;
+echo "multiplication of two no is: " .$multi;
+echo "<br><br>";
+
+// Division of two no is,
+
+$div=$no2/$no1;
+echo "Division Of Two No is : " .$div; 
+
+/*
+
+OUTPUT
+
+
+PHP Basic Addition Subtraction Multiplication Division Program!
+
+Addition Of Two Numbers is:-6
+
+Subtraction of two no is: 2
+
+multiplication of two no is: 8
+
+Division Of Two No is : 2
+
+*/
+
+
+
+
+?>
