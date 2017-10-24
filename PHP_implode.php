00994a6489a37522cbf31d2026375cefdd3319cc
Priyanka <33025640+priyanka2907@users.noreply.github.com>
2017-10-24 11:49:37 +0530

    Create PHP_implode.php

---
+++ PHP_implode.php
@@ -0,0 +1,12 @@
+<?php
+//implode();
+// it return string of array.
+
+// first declare array;
+$a=array("My","First","PHP","Program"); // define array
+
+$result=implode(' ',$a); // use implode function
+
+echo $result; // display result
+
+?>
