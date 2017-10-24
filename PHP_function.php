cfd897652b438dc4f69179fddb7c27d82e5e96c5
Priyanka <33025640+priyanka2907@users.noreply.github.com>
2017-10-24 11:47:19 +0530

    Create PHP_function.php

---
+++ PHP_function.php
@@ -0,0 +1,20 @@
+<?php
+// functions in php
+
+//define function
+
+function myfunction()
+{
+	
+	echo "<br>"."Inside Function"; //echo value
+	
+}
+
+
+myfunction();
+myfunction();// call function second time
+// call function
+
+
+//op   Inside Function
+?>
