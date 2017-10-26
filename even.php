9b45d461aa24b47802315f6030d3cc1d3b42ad0d
Priyanka <33025640+priyanka2907@users.noreply.github.com>
2017-10-26 15:38:34 +0530

    Add files via upload

---
+++ even.php
@@ -0,0 +1,39 @@
+<?php
+
+//find given no is even or odd using php;
+
+
+
+$a=2;//declare variable
+
+if($a%2==0) // check number is even 
+{
+	
+	echo "Given No is Even Number: ".$a; // display message is even number
+	
+}
+else{
+	
+	echo "Given Number is not Even Number:"; // display message if number is not even.
+	 
+}
+
+
+
+/*
+ Output 
+ 
+ Given No is Even Number: 2
+
+
+*/
+
+
+
+
+
+
+
+
+
+?>
